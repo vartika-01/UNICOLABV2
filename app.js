878696c731de4c36772a528f059382d6bbad74f8
Abinash Dutta <abinashdtt45@gmail.com>
2020-02-04 12:51:11 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//Hi This is Abinash. We are going to learn basic git skills to work on projects.
+//First we do git init and add the parent repository
+// 1. Learn to fork and clone.
+// 2. Learn to add remote head.
+// 3. Learn to fetch and rebase.
+// 4. Learn to manage conflicts.
+// 5. Learn to add, commit and push.
+// 6. Learn to generate pull request and merge with master.
---
+++ connect.js
@@ -0,0 +1,2 @@
+//Here we will be connecting to the mongo database.
+//There will be a database created named Unicolab which will have respective collections.
