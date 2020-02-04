f1f85b2d020f6aca70963fa35e2c4900353e5a8d
Abinash Dutta <abinashdtt45@gmail.com>
2020-02-04 13:07:26 +0530

    Updated Tasks for today

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 //Hi This is Abinash. We are going to learn basic git skills to work on projects.
-//First we do git init and add the parent repository
+//First we do git init and add the parent repository- not always necessary 
 // 1. Learn to fork and clone.
 // 2. Learn to add remote head.
 // 3. Learn to fetch and rebase.
 // 4. Learn to manage conflicts.
 // 5. Learn to add, commit and push.
 // 6. Learn to generate pull request and merge with master.
+
+
+//Inititial Assignment
+// 1. Arvinder Singh
+// 2. Debojit Das
+// 3. Ritu Agarwal
+// 4. Vartika Varshney
+// 5. Abinash Dutta
+
+
+//Sort of Deadline- 26th February and then before 15th March
